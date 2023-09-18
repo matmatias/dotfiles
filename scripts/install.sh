d38a4be88beb3e08ffaca54d94847bc1986317c5
SamuraX <matheusomr1@gmail.com>
2023-09-18 19:11:39 -0300

    feat(scripts): add wget, curl, unzip, gzip, packages and cascadia code nerd font to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,14 @@ INSTALL_COMMAND="install"
 
 sudo apt update
 echo "Installing packages..."
-sudo $PACKAGE_MANAGER $INSTALL_COMMAND git curl stow xclip tmux ripgrep
+sudo $PACKAGE_MANAGER $INSTALL_COMMAND git curl stow xclip tmux ripgrep wget curl unzip gzip -y
 echo "Packages installed!"
+
+echo "Installing CaskaydiaCove Nerd Font"
+sudo wget -O /usr/share/fonts/cascadiacode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
+mkdir /usr/share/fonts/cascadiacode && unzip /usr/share/fonts/cascadiacode.zip -d /usr/share/fonts/cascadiacode
+fc-cache -f -v
+echo "Font installed!"
 
 echo "Installing asdf"
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.13.1
@@ -24,7 +30,7 @@ echo "Bob installed!"
 
 echo "Insalling nvim stable"
 bob install stable
-bob use stalbe
+bob use stable
 echo "Nvim installed!"
 
 directories=()
