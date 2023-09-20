556b64c50191fd932414679caadc0f591925e42f
SamuraX <matheusomr1@gmail.com>
2023-09-20 12:48:46 -0300

    feat(nvim): add cmd completion

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -132,6 +132,7 @@ require("lazy").setup({
 			-- Code completion
 			"hrsh7th/nvim-cmp",
 			"hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-vsnip",
 			"hrsh7th/vim-vsnip",
 			-- Formatters
