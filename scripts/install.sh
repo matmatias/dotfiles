e7facd4f93144ef2f27cedd4561a03c34b4c24fb
SamuraX <matheusomr1@gmail.com>
2023-09-20 18:49:48 -0300

    fix(scripts): fix install script (works on debian only)

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,36 +2,41 @@
 
 PACKAGE_MANAGER="apt"
 INSTALL_COMMAND="install"
+UPDATE_COMMAND="update"
 
-sudo apt update
-echo "Installing packages..."
-sudo $PACKAGE_MANAGER $INSTALL_COMMAND git curl stow xclip tmux ripgrep wget curl unzip gzip -y
-echo "Packages installed!"
+mv $HOME/.bashrc $HOME/.bashrc.bak
+echo "INSTALLING PACKAGES..."
+sudo $PACKAGE_MANAGER $UPDATE_COMMAND
+sudo $PACKAGE_MANAGER $INSTALL_COMMAND -y git curl xclip tmux ripgrep wget unzip gzip dialog apt-utils build-essential stow
+echo "PACKAGES INSTALLED!"
 
-echo "Installing CaskaydiaCove Nerd Font"
+echo "INSTALLING CASKAYDIACOVE NERD FONT"
 sudo wget -O /usr/share/fonts/cascadiacode.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip
 mkdir /usr/share/fonts/cascadiacode && unzip /usr/share/fonts/cascadiacode.zip -d /usr/share/fonts/cascadiacode
 fc-cache -f -v
-echo "Font installed!"
+echo "FONT INSTALLED!"
 
-echo "Installing asdf"
+echo "INSTALLING ASDF"
 git clone https://github.com/asdf-vm/asdf.git $HOME/.asdf --branch v0.13.1
-echo "asdf installed!"
+echo "ASDF INSTALLED!"
 
-echo "Installing rust stable"
-asdf plugin-add rust https://github.com/code-lever/asdf-rust.git
-asdf install rust stable
-asdf global rust stable
-echo "Rust installed!"
+echo "INSTALLING RUST STABLE"
+ASDF_PATH="$HOME/.asdf/bin"
+"$ASDF_PATH/asdf" plugin-add rust https://github.com/code-lever/asdf-rust.git
+"$ASDF_PATH/asdf" install rust stable
+"$ASDF_PATH/asdf" global rust stable
+echo "RUST INSTALLED!"
 
-echo "Installing bob (nvim version manager)"
-cargo install bob-nvim
-echo "Bob installed!"
+echo "INSTALLING BOB (NVIM VERSION MANAGER)"
+CARGO_PATH="$HOME/.asdf/shims"
+"$CARGO_PATH/cargo" install bob-nvim
+echo "BOB INSTALLED!"
 
-echo "Insalling nvim stable"
-bob install stable
-bob use stable
-echo "Nvim installed!"
+echo "INSALLING NVIM STABLE"
+BOB_PATH="$HOME/.asdf/installs/rust/stable/bin"
+"$BOB_PATH/bob" install stable
+"$BOB_PATH/bob" use stable
+echo "NVIM INSTALLED!"
 
 directories=()
 
@@ -43,8 +48,8 @@ while IFS= read -r -d '' dir; do
   fi
 done < <(find . -mindepth 1 -maxdepth 1 -type d -print0)
 
-echo "Symlinking dotfiles"
+echo "SYMLINKING DOTFILES"
 for dir in "${directories[@]}"; do
   stow "$dir"
 done
-echo "Symlinking done"
+echo "SYMLINKING DONE"
