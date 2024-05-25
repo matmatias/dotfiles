d5089803fb60390111c6dbbe3463ac8bc4917b1e
matmatias <matheusomr1@gmail.com>
2024-05-25 14:27:32 -0300

    feat: remove dap plugins and setup

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,3 @@
 require("settings")
 require("plugins")
 require("mappings")
-
--- require('colorschemes.gruvbox')
--- require('nvim-tree-config')
--- require('autopairs')
-
--- require('lualine-config')
-
--- require('lsp-config.nvim-cmp')
--- -- Mason
--- require('lsp-config.mason')
--- require('lsp-config.null-ls')
-
--- require('treesitter-config')
-
--- require('which-key-config')
--- require('packer-config')
